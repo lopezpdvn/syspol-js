d299220d0c0b3805649e7c312677f619501cf4d8
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-01-13 21:37:12 -0600

    fs.checkDir -> fs.isDirRW

--- src/syspol-js/lib/fs.js
+++ src/syspol-js/lib/fs.js
@@ -1,13 +1,14 @@
 ﻿var fs = require('fs');
+var path = require('path');
 
-function checkDir(dirPath) {
-    //fs.accessSync(fpath, fs.R_OK | fs.W_OK);
+function isDirRW(dirPath) {
+    // fs.accessSync(fpath, fs.R_OK | fs.W_OK);
     fs.accessSync(dirPath, fs.R_OK);
     
-    // test write permissions
-    var fname = dirPath + '/dummy_file_name_ASDFFGAJSDFASDFASDF';
+    // Test write permissions
+    var fname = path.join(dirPath, '/dummy_file_name_ASDFFGAJSDFASDFASDF');
     fs.appendFileSync(fname, "DUMMY CONTENT");
     fs.unlinkSync(fname);
 }
 
-exports.checkDir = checkDir;
+exports.isDirRW = isDirRW;
