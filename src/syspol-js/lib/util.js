313504ec60092266f97892aff3c3af499a6395db
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-02-13 08:05:41 -0600

    Logging to file

--- src/syspol-js/lib/util.js
+++ src/syspol-js/lib/util.js
@@ -1,6 +1,7 @@
 ﻿var fs = require('fs');
 var util = require('util');
 var path = require('path');
+var sh = require('shelljs');
 
 var syspol_fs = require('./fs');
 var isDirRW = syspol_fs.isDirRW;
@@ -8,10 +9,10 @@ var isDirRW = syspol_fs.isDirRW;
 // Logger =============================================================
 /* Constructor/Prototype pattern */
 function Logger(loggerName, fpaths) {
-    if(!loggerName) {
+    if (!loggerName) {
         throw new Error("Logger loggerName must be supplied");
     }
-
+    
     if (fpaths) {
         fpaths.filter((fpath) => {
             var fpathDir = path.dirname(fpath);
@@ -22,18 +23,27 @@ function Logger(loggerName, fpaths) {
             return true;
         });
     }
+    this.fpaths = fpaths;
     this.loggerName = loggerName;
     this.createDate = new Date();
 }
-
-Logger.prototype.log = function(msg, severity) {
-    util.log(msg);
-};
 
 Object.defineProperty(Logger.prototype, "constructor", {
     enumerable: false,
     value: Logger
 });
+
+Logger.prototype.outputFormatDefault = "[%s %s] %s %s";
+
+Logger.prototype.log = function (msg, severity) {
+    var ISODTStr = (new Date()).toISOString();
+    var msg = util.format(this.outputFormatDefault, ISODTStr, "ORIGIN",
+        severity, msg);
+    console.log(msg);
+    this.fpaths.forEach((fpath) => {
+        msg.toEnd(fpath);
+    });
+};
 // End Logger =========================================================
 
 exports.Logger = Logger;
