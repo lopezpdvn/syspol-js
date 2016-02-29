b1e7bb398827ef204288d07c6c6dcbc39ea26448
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-02-29 03:29:20 -0600

    Added gulp task 'publish'

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var util = require('util');
 
 var gulp = require('gulp');
+var sh = require('shelljs');
 
 var syspol = require('./');
 
@@ -8,7 +9,7 @@ var pkgJSON = require('./package.json');
 var logger = new syspol.util.Logger(pkgJSON.name);
 
 function help() {
-    var tasks = ['help'];
+    var tasks = ['help', 'publish'];
     var msg = 'Tasks:\n' + tasks.join('\n');
     console.log(msg);
 }
@@ -16,3 +17,8 @@ function help() {
 gulp.task('default', help);
 gulp.task('h', help);
 gulp.task('help', help);
+
+gulp.task('publish', function() {
+    var git = sh.exec('git push origin master');
+    logger.log('Git exit code: ' + git.code);
+});
