10959a22ddfb2c8338cac1623628f58c4cd9513b
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-03-18 19:20:03 -0600

    Using npm scripts instead of gulp tasks for simple procedures

--- gulpfile.js
+++ gulpfile.js
@@ -6,26 +6,15 @@ const babel = require('gulp-babel');
 
 const syspol = require('./');
 
-var pkgJSON = require('./package.json');
-var logger = new syspol.util.Logger(pkgJSON.name);
+const pkgJSON = require('./package.json');
+const logger = new syspol.util.Logger(pkgJSON.name);
 
-function help() {
-    var tasks = ['help', 'publish'];
-    var msg = 'Tasks:\n' + tasks.join('\n');
+const help = () => {
+    const tasks = ['help'];
+    const msg = 'Tasks:\n' + tasks.join('\n');
     console.log(msg);
-}
+};
 
 gulp.task('default', help);
 gulp.task('h', help);
 gulp.task('help', help);
-
-gulp.task('publish', function() {
-    var git = sh.exec('git push origin master');
-    logger.log('Git exit code: ' + git.code);
-});
-
-gulp.task('build', function() {
-    gulp.src('src/**/*.js')
-        .pipe(babel())
-        .pipe(gulp.dest('dist'));
-});
