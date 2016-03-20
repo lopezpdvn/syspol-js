a238bdec9e72886c6f73ebb9bf5c280fd8781a6c
Pedro Ivan Lopez <m@pedroivanlopez.com>
2016-03-20 15:17:45 -0600

    `path`: Using ES6 variable declarations

--- src/lib/path.js
+++ src/lib/path.js
@@ -1,14 +1,13 @@
 ﻿'use strict'
 
-var fs = require('fs');
-var util = require('util');
-var path = require('path');
-var sh = require('shelljs');
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+const sh = require('shelljs');
 
-var randomFileName = require('./util').randomFileName;
+const randomFileName = require('./util').randomFileName;
 
-function randomTempFile() {
-    return path.resolve(path.join(sh.tempdir(), randomFileName()));
-}
+const randomTempFile = () => path.resolve(
+        path.join(sh.tempdir(), randomFileName()));
 
 exports.randomTempFile = randomTempFile;
